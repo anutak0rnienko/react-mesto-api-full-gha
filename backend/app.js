2c4d17df84b6ad02acea61d4611b893f4f4a1a8b
Anna Kornienko <anutak0rnienko@yandex.ru>
2023-09-23 13:48:09 +0300

    Практическая работа 15

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 const routes = require('./routes/router');
 const handleError = require('./middlewares/handleError');
+const { DB_ADDRESS } = require('./config');
 
 const { PORT = 3000 } = process.env;
 
@@ -23,16 +24,23 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
 }));
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/mestodb')
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('БД подключена');
-  })
-  .catch(() => {
-    // eslint-disable-next-line no-console
-    console.log('Не удалось подключиться к БД');
-  });
+// mongoose
+//   .connect('mongodb://127.0.0.1:27017/mestodb')
+//   .then(() => {
+//     // eslint-disable-next-line no-console
+//     console.log('БД подключена');
+//   })
+//   .catch(() => {
+//     // eslint-disable-next-line no-console
+//     console.log('Не удалось подключиться к БД');
+//   });
+
+mongoose.connect(DB_ADDRESS, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 app.use(bodyParser.json());
 
