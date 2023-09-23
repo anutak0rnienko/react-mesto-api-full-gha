240a375283f2e1e571732131f00f09f7e2ed4781
Anna Kornienko <anutak0rnienko@yandex.ru>
2023-09-23 13:28:01 +0300

    Практическая работа 15

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,24 @@ const bodyParser = require('body-parser');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/router');
 const handleError = require('./middlewares/handleError');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://domainname.anna.nomoredomainsrocks.ru',
+    'http://domainname.anna.nomoredomainsrocks.ru',
+  ],
+  credentials: true,
+  methods: 'GET, PUT, PATCH, POST, DELETE',
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+}));
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb')
